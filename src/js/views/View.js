53006819fe01fa6c96e8b02066860ce6a4526db4
Brandon Knight <knightbrandon1984@gmail.com>
2024-08-19 14:59:29 -0500

    added the notes

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,6 +3,16 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the recieved object to the DOM
+   * @param {Object | Oject[]} data The data to be rendered(e.g. recipe)
+   * @param {boolan} [render=true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View Instance
+   * @author Brandon Knight
+   *
+   */
+
   render(data, render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
